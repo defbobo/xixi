64e990a77971af76e8652151030eb630c1254d0c
defbobo <defbobo@gmail.com>
2017-01-19 14:37:03 +0800

    Fix some pylint issue

--- xixi/cheatsheet/__init__.py
+++ xixi/cheatsheet/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-"""The cheatsheet module, include nav bar cheatsheets filed"""
+"""The cheatsheet module, include nav bar cheatsheets filed."""
 from . import views  # noqa
--- xixi/cheatsheet/views.py
+++ xixi/cheatsheet/views.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 """User views."""
-from flask import Blueprint, render_template, abort
-
+from flask import Blueprint, abort, render_template
 
 blueprint = Blueprint('cheatsheet', __name__, url_prefix='/cheatsheet', static_folder='../static')
 
 
 @blueprint.route('/')
 def home():
-    """List members."""
+    """Cheatsheet home."""
     return render_template('cheatsheets/home.html')
 
-@blueprint.route('/<string:page>', methods=['GET'])
-def get_cheatsheet(page):
+
+@blueprint.route('/<string:id>', methods=['GET'])
+def get_cheatsheet(id):
+    """Return cs page by id."""
     try:
-        return render_template('cheatsheets/{0}.html'.format(page))
-    except:
+        return render_template('cheatsheets/{0}.html'.format(id))
+    except Exception:
         abort(404)
