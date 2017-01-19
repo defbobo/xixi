13daa91b054101abf77d210fd69251da7cbc7dd2
defbobo <defbobo@gmail.com>
2017-01-19 09:35:47 +0800

    update autoapp: update according project env

--- autoapp.py
+++ autoapp.py
@@ -2,8 +2,8 @@
 """Create an application instance."""
 from flask.helpers import get_debug_flag
 
-from app.app import create_app
-from app.settings import DevConfig, ProdConfig
+from xixi.app import create_app
+from xixi.settings import DevConfig, ProdConfig
 
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
 
