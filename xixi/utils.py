1bd65a348bfa435233bee23573e83114c3fbe494
defbobo <defbobo@gmail.com>
2017-01-23 13:16:03 +0800

    add read_resume function

--- xixi/utils.py
+++ xixi/utils.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Helper utilities and decorators."""
 from flask import flash
+import json
+import os
 
 
 def flash_errors(form, category='warning'):
@@ -8,3 +10,7 @@ def flash_errors(form, category='warning'):
     for field, errors in form.errors.items():
         for error in errors:
             flash('{0} - {1}'.format(getattr(form, field).label.text, error), category)
+
+def read_resume(resume_path):
+    print(os.getcwd())
+    return json.load(open(resume_path))
